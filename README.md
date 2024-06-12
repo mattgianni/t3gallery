c646947db7acd0a7f3445d1605fa4934423848a4
Matthew Gianni <matt@gianni.org>
2024-06-12 00:12:28 -0700

    basic ui with mock data

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## TODO
 
-- [ ] Make it deploy
-- [ ] Scaffold basic ui with mock data
+- [x] Make it deploy
+- [x] Scaffold basic ui with mock data
 - [ ] Set up DB
 - [ ] Attach DB to UI
 - [ ] Add authentication
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+// import Image from "next/image";
 import Link from "next/link";
+
+const mockUrls = [
+    "https://gianni.org/brightstart-vite/painting.jpg",
+    "https://gianni.org/brightstart-vite/mj_boy_smiling.png",
+    "https://gianni.org/brightstart-vite/book.jpg",
+    "https://gianni.org/brightstart-vite/reading.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
 
 export default function HomePage() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <main className="">
+            <div className="flex flex-wrap gap-2">
+                {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+                    <div key={image.id} className="w-48">
+                        <img src={image.url} alt="image" className="w-full" />
+                    </div>
+                ))}
+            </div>
             Hello (gallery in progress)
         </main>
     );
